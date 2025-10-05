58e7e19a2a9df7b2917399af17ff85a601987259
Tu Nombre <lgarbate@gmail.com>
2025-10-05 19:38:20 -0300

    nasa

--- komet/next.config.ts
+++ komet/next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  eslint: {
+    // During production builds, Next.js runs ESLint by default and will
+    // fail the build on lint errors. Set this to true to skip ESLint checks
+    // during `next build`.
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
